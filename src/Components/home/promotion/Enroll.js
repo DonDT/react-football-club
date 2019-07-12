5cd311f889bd27342a68af030ba755edee0cccc9
Donald Njumbe Ediage <ediage@yahoo.com>
2019-07-12 15:43:14 +0300

    Adding Form

---
+++ src/Components/home/promotion/Enroll.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import Fade from "react-reveal/Fade";
+
+class Enroll extends Component {
+  state = {
+    formError: false,
+    formSuccess: "",
+    formdata: {
+      email: {
+        element: "input",
+        value: "",
+        config: {
+          name: "email_input",
+          type: "email",
+          placeholder: "Enter your email"
+        },
+        validation: {
+          required: true,
+          email: true
+        },
+        valid: false,
+        validationMessage: ""
+      }
+    }
+  };
+
+  render() {
+    return <Fade>Here</Fade>;
+  }
+}
+
+export default Enroll;
